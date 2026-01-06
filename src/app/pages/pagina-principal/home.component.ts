1f1e5ecd2374a9682cbd1ee89195855a9e48cffb
lgustavori <luiz.vieira@engdb.com.br>
2026-01-06 11:31:42 -0300

    correccao erros vercel

--- src/app/pages/pagina-principal/home.component.ts
+++ src/app/pages/pagina-principal/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { CarouselComponent } from '../../components/cabecalho/cabecalho.component';
+import { CarouselComponent } from '../../components/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, CarouselComponent],
+  imports: [CommonModule, RouterModule,CarouselComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
